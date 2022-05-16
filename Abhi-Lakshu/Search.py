6c05206aa2b0ba8dd1c448a0b4f54e943a3103ff
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-16 19:50:11 +0530

    Update Search.py

--- Abhi-Lakshu/Search.py
+++ Abhi-Lakshu/Search.py
@@ -1,1 +1,23 @@
+from Helper import page_soup
 
+def get_lyrics(page_soup: Soup):
+    # containerize
+    containers = page_soup.find_all("div", {"class": "BNeawe tAd8D AP7Wnd"})
+    lyrics = []
+
+    for i in range(0, len(containers), 2):
+        lyrics.append(containers[i].text)
+    lyrics_str = "".join([str(x) for x in lyrics]).strip("\n")
+    return lyrics_str
+
+
+def get_artist(page_soup: Soup):
+    # containerize
+    containers = page_soup.find_all("span", {"class": "BNeawe s3v9rd AP7Wnd"})
+    return containers[1].text
+
+
+def get_title(page_soup: Soup):
+    # containerize
+    containers = page_soup.find_all("span", {"class": "BNeawe tAd8D AP7Wnd"})
+    return containers[0].text
