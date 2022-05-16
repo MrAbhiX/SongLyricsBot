eb199e0cbc2d9757e5f4824c86dabd12c614f05d
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-16 20:08:41 +0530

    Update __init__.py

--- Abhi-Lakshu/__init__.py
+++ Abhi-Lakshu/__init__.py
@@ -1,2 +1,12 @@
 from pyrogram import Client
 from .Config import Configs
+
+
+config = Config()
+
+Bot = Client(
+    ":SongLyricsBot:",
+    config.API_ID,
+    config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+  )
