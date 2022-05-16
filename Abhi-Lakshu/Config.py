840f5b5c6e4bd8980a81f2ea9b1e3f101a2f3516
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-16 20:03:21 +0530

    Update Config.py

--- Abhi-Lakshu/Config.py
+++ Abhi-Lakshu/Config.py
@@ -17,4 +17,4 @@ class Configs:
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
     OWNER_ID = int(getenv("OWNER_ID", "2117545440"))    
     CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/vexera_updates")
-    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/live_life_like")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/Heaven_of_friends")
