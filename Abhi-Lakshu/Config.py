c27b8a7dc415e1825618446df1090b4b879e3a63
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-16 19:59:42 +0530

    Create Config.py

---
+++ Abhi-Lakshu/Config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from os import path, getenv, mkdir
+
+
+if path.exists("local.env"):
+    load_dotenv("local.env")
+else:
+    load_dotenv()
+
+if not path.exists("search"):
+    mkdir("search")
+    
+    
+class Configs:
+    API_ID = int(getenv("API_ID", "0"))
+    API_HASH = getenv("API_HASH", "abc123")
+    BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
+    OWNER_ID = int(getenv("OWNER_ID", "2117545440"))    
+    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/vexera_updates")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/live_life_like")
