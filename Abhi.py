c12a5ea6da65be36d306d595bd2dc0e6a2439328
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-17 21:36:08 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
-
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
 from urllib.request import urlopen as u_reqs, Request
 from bs4 import BeautifulSoup as Soup
 from pyrogram.types import Message
@@ -14,5 +15,32 @@ bot = Client(
 )
 
 
+aiohttpsession = ClientSession()
 
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
+@bot.on_message(filters.command("lyrics") & ~filters.edited)
+async def lyrics_func(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Usage:**\n/lyrics [QUERY]")
+    m = await message.reply_text("**Searching**")
+    query = message.text.strip().split(None, 1)[1]
+
+    resp = await arq.lyrics(query)
+
+    if not (resp.ok and resp.result):
+        return await m.edit("No lyrics found.")
+
+    song = resp.result[0]
+    song_name = song['song']
+    artist = song['artist']
+    lyrics = song['lyrics']
+    msg = f"**{song_name}** | **{artist}**\n\n__{lyrics}__"
+          
+    if len(msg) > 4095:
+        msg = await paste(msg)
+        msg = f"**LYRICS_TOO_LONG:** [URL]({msg})"
+    return await m.edit(msg)
+
+bot.start()
+print("BOT STARTED SUCCESSFULLY")
